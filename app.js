26cb5a63fa074caf00b2775ded909e367e0d78f2
aksh-t <tanno.ao@gmail.com>
2021-02-02 21:15:08 +0900

    serve static posts html

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!!!!!!!!')
+  res.send(`
+  <h1>buri</h1>
+  <ul>
+  <li>すごい</li>
+  <li>あとで</li>
+  <li>グッド</li>
+  </ul>
+  `)
 })
 
 app.listen(port, () => {
