4666ef0a4dcdbfa564c6cf76dd01c1116a2e6223
aksh-t <tanno.ao@gmail.com>
2021-02-10 21:23:30 +0900

    投稿の入力フォームを作成

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 let posts = fs.readFileSync("data/posts.txt", 'utf-8');
 const postList = posts.split('\n');
-const liTags = postList.map(x => `<li>${x}</li>`)
+const liTags = postList.map(x => `<li>${x}</li>`);
 console.log(liTags.join(''));
 
 app.get('/', (req, res) => {
@@ -15,9 +15,14 @@ app.get('/', (req, res) => {
   <ul>
     ${liTags.join('')}
   </ul>
-  `)
-})
+  <form action="" method="get">
+    <input type="text" id="post" name="post" required
+        minlength="1" maxlength="1000" size="30">
+    <input type="submit" value="Subscribe!">
+  </form>
+  `);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
