9651966766f2db5542cab732f4604c386edfa259
babyhotate-4 <79208530+babyhotate-4@users.noreply.github.com>
2021-05-11 20:50:18 +0900

    getByIdsを実装

--- userRepository.js
+++ userRepository.js
@@ -28,6 +28,31 @@ class UserRepository {
     const user = users.find(user => user.id === id);
     return user;
   }
+
+getByIds(ids) {
+  if (!fs.existsSync(this.filePath)) {
+    fs.writeFileSync(this.filePath, "");
+  }
+
+  // データストアの全データを読み出す
+  let data = fs.readFileSync(this.filePath, 'utf-8');
+  const lines = data.split('\n').filter(value => value !== "");
+
+  // 読み出したデータをUserオブジェクトに変換し、リストに詰める
+  let users = [];
+  for (const line of lines) {
+    const values = line.split(',');
+    users.push(new User(values[0], values[1]));
+  }
+
+  // Userオブジェクトのリストから指定されたIDのユーザを探し出す
+  let filteredUsers = [];
+  for (const id of ids) {
+    const user = users.find(user => user.id === id);
+    filteredUsers.push(user);
+  }
+  return filteredUsers;
+}
 }
 
 module.exports.UserRepository = UserRepository;
