2636b77a2ea1d714c1509ec474163f9c6a40530d
babyhotate-4 <79208530+babyhotate-4@users.noreply.github.com>
2021-05-11 21:02:08 +0900

    format

--- userRepository.test.js
+++ userRepository.test.js
@@ -14,15 +14,15 @@ test('getByIdのテスト', () => {
 describe('#getByIds', () => {
     test('userを取得できる', () => {
         fs.writeFileSync(USERS_FILE_PATH, ["user1,hotate", "user2,hamachi"].join("\n"));
-        
+
         const userRepository = new UserRepository(DATA_DIR_PATH);
         const userIds = ["user1", "user2"];
         const users = userRepository.getByIds(userIds);
-        
+
         expect(users.length).toBe(2);
         expect(users[0].id).toBe("user1");
         expect(users[0].displayName).toBe("hotate");
         expect(users[1].id).toBe("user2");
         expect(users[1].displayName).toBe("hamachi");
-    } );
+    });
 });
