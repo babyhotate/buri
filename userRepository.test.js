27201e121d90f2eba6fa9d8427d38603ffb0f825
babyhotate-4 <79208530+babyhotate-4@users.noreply.github.com>
2021-05-11 20:31:40 +0900

    getUserIdsのテストを追加

--- userRepository.test.js
+++ userRepository.test.js
@@ -1,10 +1,28 @@
 const { UserRepository } = require('./userRepository');
+const fs = require('fs');
 
 const DATA_DIR_PATH = 'test/data';
+const USERS_FILE_PATH = `${DATA_DIR_PATH}/users.txt`;
 
 test('getByIdのテスト', () => {
     const userRepository = new UserRepository(DATA_DIR_PATH);
     const user = userRepository.getById('user2');
     expect(user.id).toBe("user2");
     expect(user.displayName).toBe("hamachi");
+});
+
+describe('#getByIds', () => {
+    test('userを取得できる', () => {
+        fs.writeFileSync(USERS_FILE_PATH, ["user1,hotate", "user2,hamachi"].join("\n"));
+        
+        const userRepository = new UserRepository(DATA_DIR_PATH);
+        const userIds = ["user1", "user2"];
+        const users = userRepository.getByIds(userIds);
+        
+        expect(users.length).toBe(2);
+        expect(users[0].id).toBe("user1");
+        expect(users[0].displayName).toBe("hotate");
+        expect(users[1].id).toBe("user2");
+        expect(users[1].displayName).toBe("hamachi");
+    } );
 });
