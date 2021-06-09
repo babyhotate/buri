f3b717eb439dde5859003b4b03ef7f3ddf43fa6c
babyhotate-4 <79208530+babyhotate-4@users.noreply.github.com>
2021-06-09 20:55:13 +0900

    liタグの削除処理

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,6 +1,24 @@
 setInterval(() => {
 }, 1000);
 
+// TODO: 関数の仕事が決まったら変えること
+function apple(posts) {
+    console.log(posts);
+
+    // TODO: 要らなくなったら消すこと
+    const answer = confirm("DOMを更新してもいいですか？");
+    console.log("🍣", answer);
+    if (!answer) {
+        return;
+    }
+
+    const ulElement = document.getElementById('posts');
+    while (ulElement.firstChild) {
+        ulElement.removeChild(ulElement.firstChild);
+    }
+
+};
+
 fetch('http://localhost:3000/api/posts')
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => apple(data));
