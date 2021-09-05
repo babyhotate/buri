5298cb9bb8e347856869bfc592a94a9002096876
貝津志織 <kaizushiori@kaitsushiorinoMacBook-Air.local>
2021-09-05 13:56:07 +0900

    発言者・メッセージ・削除ボタン・編集ボタンを一行に表示する

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -16,27 +16,10 @@ function apple(posts) {
         const li = document.createElement('li');
         li.textContent = post['user']['displayName'];
 
-        const formForDelete = document.createElement('form');
-        formForDelete.setAttribute('action', '/delete_post');
-        formForDelete.setAttribute('method', 'get');
-
-        const hiddenForDelete = document.createElement('input');
-        hiddenForDelete.setAttribute('type', 'hidden');
-        hiddenForDelete.setAttribute('value', '0');
-        hiddenForDelete.setAttribute('id', 'post');
-        hiddenForDelete.setAttribute('name', 'post_id');
-        formForDelete.appendChild(hiddenForDelete);
-
-        const submitForDelete = document.createElement('input');
-        submitForDelete.setAttribute('type', 'submit');
-        submitForDelete.setAttribute('value', '廃棄');
-        formForDelete.appendChild(submitForDelete);
-
-        li.appendChild(formForDelete);
-
         const formForEdit = document.createElement('form');
         formForEdit.setAttribute('action', '/edit_post');
         formForEdit.setAttribute('method', 'get');
+        formForEdit.setAttribute('style', 'display: inline');
 
         const textForEdit = document.createElement('input');
         textForEdit.setAttribute('type', 'text');
@@ -57,6 +40,25 @@ function apple(posts) {
         formForEdit.appendChild(submitForEdit);
 
         li.appendChild(formForEdit);
+
+        const formForDelete = document.createElement('form');
+        formForDelete.setAttribute('action', '/delete_post');
+        formForDelete.setAttribute('method', 'get');
+        formForDelete.setAttribute('style', 'display: inline');
+
+        const hiddenForDelete = document.createElement('input');
+        hiddenForDelete.setAttribute('type', 'hidden');
+        hiddenForDelete.setAttribute('value', '0');
+        hiddenForDelete.setAttribute('id', 'post');
+        hiddenForDelete.setAttribute('name', 'post_id');
+        formForDelete.appendChild(hiddenForDelete);
+
+        const submitForDelete = document.createElement('input');
+        submitForDelete.setAttribute('type', 'submit');
+        submitForDelete.setAttribute('value', '廃棄');
+        formForDelete.appendChild(submitForDelete);
+
+        li.appendChild(formForDelete);
 
         ulElement.appendChild(li);
     }
