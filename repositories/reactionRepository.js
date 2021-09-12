e69fb3958c50ee9ee778b1ecd9624548b1f3cf5b
mayumi aka inukawaii <inukawaii999@gmail.com>
2021-09-12 17:00:57 +0900

    特定のPOSTに紐づくリアクションのリストを取得するメソッドを追加

---
+++ repositories/reactionRepository.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+
+class ReactionRepository {
+  filePath;
+
+  constructor(dataDirPath) {
+    this.filePath = `${dataDirPath}/reactions.txt`;
+  }
+
+  getByPostId(postId) {
+    if (!fs.existsSync(this.filePath)) {
+      fs.writeFileSync(this.filePath, "");
+    }
+    let reactions = fs.readFileSync(this.filePath, "utf-8");
+
+    // ["", "aaa,bbb", ""] => [["aaa,bbb"]]
+    const reactionList = reactions.split("\n").filter((value) => value !== "").map((value) => value.split(","));
+
+    return reactionList
+      .filter((list) => list[1] === String(postId))
+      .map((r) => {
+        return { emoji: r[0], userId: r[2] };
+      });
+  }
+}
+
+
+
+
+module.exports.ReactionRepository = ReactionRepository;
