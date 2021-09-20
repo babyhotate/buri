e263fad5bda6e344c8af3753cd2dbdf894886496
mayumi aka inukawaii <inukawaii999@gmail.com>
2021-09-20 18:24:40 +0900

    format document

--- repositories/postRepository.js
+++ repositories/postRepository.js
@@ -1,72 +1,90 @@
-const fs = require('fs');
-const { Post } = require('../models/post');
+const fs = require("fs");
+const { Post } = require("../models/post");
 
 class PostRepository {
-    filePath;
+  filePath;
 
-    constructor(dataDirPath) {
-        this.filePath = `${dataDirPath}/posts.txt`;
+  constructor(dataDirPath) {
+    this.filePath = `${dataDirPath}/posts.txt`;
+  }
+
+  findAll() {
+    if (!fs.existsSync(this.filePath)) {
+      fs.writeFileSync(this.filePath, "");
     }
+    let posts = fs.readFileSync(this.filePath, "utf-8");
 
-    findAll() {
-        if (!fs.existsSync(this.filePath)) {
-            fs.writeFileSync(this.filePath, "");
-        }
-        let posts = fs.readFileSync(this.filePath, 'utf-8');
+    // ["", "aaa", ""] => ["aaa"]
+    const postList = posts.split("\n").filter((value) => value !== "");
 
-        // ["", "aaa", ""] => ["aaa"] 
-        const postList = posts.split('\n').filter(value => value !== "");
+    // 文字列のリストから、Postモデルのリストを作る
+    const models = postList.map((post) => {
+      const [userId, message, id] = post.split(",");
+      return new Post(userId, message, id);
+    });
+    return models;
+  }
 
-        // 文字列のリストから、Postモデルのリストを作る
-        const models = postList.map(post => {
-            const [userId, message, id] = post.split(",");
-            return new Post(userId, message, id);
-        });
-        return models;
+  create(userId, message) {
+    const postList = this.findAll();
+    const latestId = postList[postList.length - 1].id;
+    const post = new Post(userId, message, Number(latestId) + 1);
+    try {
+      const postList = this.findAll();
+      // なんかよくわからないが空ファイルも、1行の空行があるように見えてしまう
+      // ので、全くの空かどうかで書き込み方を切り替える必要がある
+      if (postList.length > 0) {
+        fs.appendFileSync(
+          this.filePath,
+          "\n" + post.userId + "," + post.message + "," + post.id
+        );
+      } else {
+        fs.appendFileSync(
+          this.filePath,
+          post.userId + "," + post.message + "," + post.id
+        );
+      }
+    } catch (e) {
+      console.log(e.message);
     }
+  }
 
-    create(userId, message) {
-        const postList = this.findAll();
-        const latestId = postList[postList.length - 1].id;
-        const post = new Post(userId, message, Number(latestId) + 1);
-        try {
-          const postList = this.findAll();
-          // なんかよくわからないが空ファイルも、1行の空行があるように見えてしまう
-          // ので、全くの空かどうかで書き込み方を切り替える必要がある
-          if (postList.length > 0) {
-            fs.appendFileSync(this.filePath, "\n" + post.userId + "," + post.message + "," + post.id);
-          } else {
-            fs.appendFileSync(this.filePath, post.userId + "," + post.message + "," + post.id);
-          }
-        } catch (e) {
-          console.log(e.message);
-        }
-    }
+  // @params index {Number}
+  delete(index) {
+    const postList = this.findAll();
+    postList.splice(index, 1);
+    const postStringLines = postList.map(
+      (p) => p.userId + "," + p.message + "," + p.id
+    );
+    const lines = postStringLines.join("\n");
 
-    // @params index {Number}
-    delete(index) {
-        const postList = this.findAll();
-        postList.splice(index, 1);
-        const postStringLines = postList.map((p) => p.userId + "," + p.message + "," + p.id);
-        const lines = postStringLines.join("\n");
+    fs.writeFileSync(this.filePath, lines, function (err) {
+      if (err) {
+        throw err;
+      }
+    });
+  }
 
-        fs.writeFileSync(this.filePath, lines, function (err) {
-            if (err) { throw err; }
-        });
-    }
+  // @params index {Number}
+  update(index, edit_content) {
+    const postList = this.findAll();
+    const editedPost = new Post(
+      postList[index].userId,
+      edit_content,
+      postList[index].id
+    );
+    postList[index] = editedPost;
+    const postStringLines = postList.map(
+      (p) => p.userId + "," + p.message + "," + p.id
+    );
+    const lines = postStringLines.join("\n");
 
-    // @params index {Number}
-    update(index, edit_content) {
-        const postList = this.findAll();
-        const editedPost = new Post(postList[index].userId, edit_content, postList[index].id);
-        postList[index] = editedPost;
-        const postStringLines = postList.map((p) => p.userId + "," + p.message + "," + p.id);
-        const lines = postStringLines.join("\n");
-
-        fs.writeFileSync(this.filePath, lines, function (err) {
-            if (err) { throw err; }
-        });
-    }
+    fs.writeFileSync(this.filePath, lines, function (err) {
+      if (err) {
+        throw err;
+      }
+    });
+  }
 }
 
 module.exports.PostRepository = PostRepository;
--- test/postRepository.test.js
+++ test/postRepository.test.js
@@ -1,28 +1,31 @@
-const { PostRepository } = require('../repositories/postRepository');
-const { Post } = require('../models/post');
-const fs = require('fs');
+const { PostRepository } = require("../repositories/postRepository");
+const { Post } = require("../models/post");
+const fs = require("fs");
 
-const DATA_DIR_PATH = 'test/data';
+const DATA_DIR_PATH = "test/data";
 const POSTS_FILE_PATH = `${DATA_DIR_PATH}/posts.txt`;
 
 let postRepository;
 beforeEach(() => {
-    postRepository = new PostRepository(DATA_DIR_PATH);
-    // posts.txtをテストしたい状態にする
-    fs.writeFileSync(POSTS_FILE_PATH, ["user1,hoge,1", "user2,fuga,2"].join("\n"));
+  postRepository = new PostRepository(DATA_DIR_PATH);
+  // posts.txtをテストしたい状態にする
+  fs.writeFileSync(
+    POSTS_FILE_PATH,
+    ["user1,hoge,1", "user2,fuga,2"].join("\n")
+  );
 });
 
-describe('#findAll', () => {
-    test('post全件が取得できる', () => {
-        const posts = postRepository.findAll();
-        expect(posts.length).toBe(2);
-        expect(posts[0].userId).toBe("user1");
-        expect(posts[0].message).toBe("hoge");
-        expect(posts[0].id).toBe("1");
-        expect(posts[1].userId).toBe("user2");
-        expect(posts[1].message).toBe("fuga");
-        expect(posts[1].id).toBe("2");
-    });
+describe("#findAll", () => {
+  test("post全件が取得できる", () => {
+    const posts = postRepository.findAll();
+    expect(posts.length).toBe(2);
+    expect(posts[0].userId).toBe("user1");
+    expect(posts[0].message).toBe("hoge");
+    expect(posts[0].id).toBe("1");
+    expect(posts[1].userId).toBe("user2");
+    expect(posts[1].message).toBe("fuga");
+    expect(posts[1].id).toBe("2");
+  });
 });
 
 describe("#create", () => {
@@ -35,25 +38,25 @@ describe("#create", () => {
   });
 });
 
-describe('#delete', () => {
-    test('1件の投稿を削除する', () => {
-        postRepository.delete(1);
+describe("#delete", () => {
+  test("1件の投稿を削除する", () => {
+    postRepository.delete(1);
 
-        let posts = fs.readFileSync(POSTS_FILE_PATH, 'utf-8');
-        posts = posts.split("\n");
+    let posts = fs.readFileSync(POSTS_FILE_PATH, "utf-8");
+    posts = posts.split("\n");
 
-        expect(posts).not.toContain('user2,fuga1,2');
-        expect(posts).toContain('user1,hoge,1');
-    });
+    expect(posts).not.toContain("user2,fuga1,2");
+    expect(posts).toContain("user1,hoge,1");
+  });
 });
 
-describe('#update', () => {
-    test('1件の投稿を編集する', () => {
-        postRepository.update(1, 'fuga2');
+describe("#update", () => {
+  test("1件の投稿を編集する", () => {
+    postRepository.update(1, "fuga2");
 
-        let posts = fs.readFileSync(POSTS_FILE_PATH, 'utf-8');
-        posts = posts.split("\n");
+    let posts = fs.readFileSync(POSTS_FILE_PATH, "utf-8");
+    posts = posts.split("\n");
 
-        expect(posts[1]).toBe("user2,fuga2,2");
-    });
+    expect(posts[1]).toBe("user2,fuga2,2");
+  });
 });
