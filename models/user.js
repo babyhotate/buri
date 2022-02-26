77a2d9193da74e4a14253c4b9bb8ed98007e986e
aksh-t <tanno.ao@gmail.com>
2022-02-26 15:21:26 +0900

    Userモデルにpasswordプロパティを追加

--- models/user.js
+++ models/user.js
@@ -2,11 +2,13 @@ class User {
   userId;
   id;
   displayName;
+  password;
 
-  constructor({ id, userId, displayName }) {
+  constructor({ id, userId, displayName, password }) {
     this.id = id;
     this.userId = userId;
     this.displayName = displayName;
+    this.password = password;
   }
 }
 
